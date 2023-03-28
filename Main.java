145522ab9bba9f6ef086cf216b8b1c87fe6c8a4b
Manash Kamal Dey <98301998+Manausk@users.noreply.github.com>
2023-03-28 23:09:46 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Single line comment
+        /*
+        Multi-
+        Line-
+        Comment
+         */
+        String name = "Manas";
+        System.out.println(name);
+
+        int number = 20;
+        System.out.println(number);
+
+        float myNumber = 7.66f;
+        System.out.println(myNumber);
+
+        boolean bool = true;
+        System.out.println(bool);
+
+        char charrr = 'D';
+        System.out.println(charrr);
+
+        int myInt = 9;
+        double myDouble = myInt;
+        System.out.println(myInt);
+        System.out.println(myDouble);
+
+
+
+
+//        System.out.println(2+4);
+    }
+}
